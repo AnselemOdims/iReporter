a5729bcf9b8a208c5c33e5f31214c3112f254ce5
Anselem77 <anselemodims@gmail.com>
2018-11-30 08:12:23 +0100

    feat(edit-red-flag) Edit  red-flags feature

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -17,7 +17,7 @@ app.post('/api/v1/login', validateLoginUser, UserController.loginUser);
 app.post('/api/v1/create-red-flag', auth.verifyUserToken, validateRedFlagReport, ReportController.createRedFlag);
 app.get('/api/v1/get-red-flags', auth.verifyUserToken, ReportController.getAllRedFlags);
 app.get('/api/v1/get-red-flag/:id', auth.verifyUserToken, ReportController.getSingleRedFlag);
-// app.put('/api/v1/edit-red-flag/:id', ReportController.editRedFlag);
+app.put('/api/v1/edit-red-flag/:id', auth.verifyUserToken, validateRedFlagReport, ReportController.editRedFlag);
 // app.delete('/api/v1/delete-red-flag/:id', ReportController.deleteRedFlag);
 
 
